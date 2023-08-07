99204841a0bac95d69303217d5ecf743e4f22900
thenPie <118397975+thenPie@users.noreply.github.com>
2023-08-07 22:07:44 +0300

    original

---
+++ task2/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
