e0631101f31b8d4529fab3a75095ed38550bd75f
thenPie <118397975+thenPie@users.noreply.github.com>
2023-08-08 16:27:13 +0300

    did t2

--- task2/src/App.java
+++ task2/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
         try {
             int d = 0;
             double catchedRes1 = intArray[8] / d;
--- task3/src/App.java
+++ task3/src/App.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
 
+    // Дан следующий код, исправьте его там, где требуется
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -11,14 +13,15 @@ public class App {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable ex) {
+        } catch (Throwable e) {
             System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
+        } /* catch (NullPointerException e) {
             System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Массив выходит за пределы своего размера!");
         }
-        
+        */
+
     }
 
     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
