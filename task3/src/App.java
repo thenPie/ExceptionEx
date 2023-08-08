b5a940c22593b5d8ee0327f4f139f4e84ca2227d
thenPie <118397975+thenPie@users.noreply.github.com>
2023-08-08 16:30:43 +0300

    did t3

--- task3/src/App.java
+++ task3/src/App.java
@@ -13,14 +13,13 @@ public class App {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable e) {
-            System.out.println("Что-то пошло не так...");
-        } /* catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable e) {
+            System.out.println("Что-то пошло не так...");
         }
-        */
 
     }
 
