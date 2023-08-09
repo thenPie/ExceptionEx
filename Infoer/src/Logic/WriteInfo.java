d7d5d6896fdcf39fedfb4e3e57dfc15373dae91e
thenPie <118397975+thenPie@users.noreply.github.com>
2023-08-09 21:38:56 +0300

    did writing and checking for last name

--- Infoer/src/Logic/WriteInfo.java
+++ Infoer/src/Logic/WriteInfo.java
@@ -1,6 +1,5 @@
 package Logic;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,14 +7,30 @@ import java.nio.file.Paths;
 
 public class WriteInfo {
     
-    public static void Write(String[] info) {
+    public static void Write(String[] info) throws IOException {
 
         String fileName = info[0] + ".txt";
         String fileOutputPath = "src/Files/" + fileName;
 
-        // writer.write("<" + info[0] + "><" + info[1] + "<>" + info[2] + "<>" + info[3] + "<>" + info[4] + "<>" + info[5] + "<");
-
         boolean fileExists = Files.exists(Paths.get(fileOutputPath));
+        DoesFileExist(fileExists, fileOutputPath);
+        Writeer(info, fileOutputPath);
+
+    }
+    
+    private static void DoesFileExist(boolean fileExists, String fileOutputPath) throws IOException {
+
+        if (fileExists == false) {
+            Files.createFile(Paths.get(fileOutputPath));
+        }
+
+    }
+    
+    private static void Writeer(String[] info, String fileOutputPath) throws IOException {
+
+        try (FileWriter writer = new FileWriter(fileOutputPath, true)) {
+            writer.write("<" + info[0] + "><" + info[1] + "><" + info[2] + "><" + info[3] + "><" + info[4] + "><" + info[5] + ">\n");
+        }
         
     }
 
